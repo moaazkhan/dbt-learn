b03e2082ec91c01f6fa7e220ff08f04b3f6e84dd
Moaaz Khan <moaazkhan33@gmail.com>
2024-09-29 22:15:42 +0000

    loaded_at field setup, adding sources and their freshness flags.

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,40 +1,29 @@
-
-WITH customers as (
-
-SELECT * FROM {{ ref('stg_jaffle_shop__customers') }}
-
+with customers as (
+    select * from {{ ref ('stg_jaffle_shop__customers')}}
 ),
-
 orders as (
-SELECT * FROM {{ ref('stg_jaffle_shop__orders') }}
+    select * from {{ ref ('fct_orders')}}
 ),
-
-
 customer_orders as (
-    SELECT 
+    select
         customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min (order_date) as first_order_date,
+        max (order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
     group by 1
 ),
-
-
-final as (
-    SELECT 
-        c.customer_id,
-        c.first_name,
-        c.last_name,
-        co.first_order_date,
-        co.most_recent_order_date,
-        ZEROIFNULL(co.number_of_orders) as number_of_orders
-
-    from customers c
-
-    left join customer_orders co
-    ON c.customer_id = co.customer_id
-
+ final as (
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce (customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+    from customers
+    left join customer_orders using (customer_id)
 )
-
-SELECT * FROM final
+select * from final
--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -3,4 +3,4 @@ SELECT
     first_name,
     last_name
 
-from raw.jaffle_shop.customers
+from {{ source('jaffle_shop', 'customers') }}
